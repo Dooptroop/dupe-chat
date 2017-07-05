b5aca6cf3955562d3fe7dea0d17e66ce209289d1
mdupree <mikerdupree@gmail.com>
2017-07-04 18:47:58 -0700

    message emit from client to server, server sends the message to all clients

--- index.js
+++ index.js
@@ -12,11 +12,14 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/' + html_dir + 'index.html');
 });
 
-
+//After connection is made
 io.on('connection', function(socket){
-    console.log('a user connected');
-});
 
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+
+});
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
