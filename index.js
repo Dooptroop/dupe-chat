db523a929fbc094c33c1d69b5b93547fa51f0fe3
mdupree <mikerdupree@gmail.com>
2017-07-04 18:35:38 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 /**
  * Created by mdupree on 04/07/17.
  */
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var html_dir = 'templates/';
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/' + html_dir + 'index.html');
+});
+
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
